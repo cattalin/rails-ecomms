fe3f5c257a34bd440e07d2200b653226688d379e
Angheluta Catalin <catalin.angheluta@my.fmi.unibuc.ro>
2018-01-17 09:42:00 +0200

    category pages and references from products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
+  before_action :set_opinion, only: [:show]
+  
+  
+  def index
+    @categories = Category.all
+  end
+  
   def show
   end
-
-  def index
-  end
+  
+  
+  private
+  
+    def set_opinion
+      @category = Category.find(params[:id])
+    end
 end
